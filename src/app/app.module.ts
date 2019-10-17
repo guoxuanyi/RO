e3125c56a372f2d5646370a92f426cd0c261978d
Guo <1620612414@qq.com>
2019-10-17 17:50:17 +0800

    RO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, SimpleChanges, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  menu: any = [
+    { id: 1, title: 'Home', isActive: true },
+    { id: 2, title: 'My Requests', isActive: false },
+    { id: 3, title: 'My Queries', isActive: false },
+    { id: 4, title: 'Trade Compliance', isActive: false },
+    { id: 5, title: 'References', isActive: false },
+    { id: 6, title: 'FAQs', isActive: false }
+  ];
+  expandButton: boolean = true;
+  expandMenu() {
+    this.expandButton = !this.expandButton;
+  }
+  changMenuFn(item) {
+    this.menu.forEach(element => {
+      if (element.id == item.id) {
+        element.isActive = true;
+      }
+      else {
+        element.isActive = false;
+      }
+    });
+  }
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+
+}
